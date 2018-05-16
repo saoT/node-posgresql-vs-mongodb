57b582d09fe900bbc48f0032aa6254f64b6fda30
saraguerric <sara.guerric@evergig.com>
2018-05-16 17:25:03 +0200

    Add: foreign key case scenario

--- index.js
+++ index.js
@@ -22,11 +22,44 @@ const dbSQL = require('./psql/connection.js');
 
   // Select owners
   const getOwnerInfosNatural = await dbSQL.query('select * from owners natural inner join users');
-  const getOwnerInfosExplicit = await dbSQL.query('select * from owners natural inner join users on owners.user_id = users.user_id');
-  console.log(getOwnerInfosNatural);
-  console.log(getOwnerInfosExplicit);
+  const getOwnerInfosExplicit = await dbSQL.query('select * from owners inner join users on owners.user_id = users.user_id');
+  console.log(getOwnerInfosNatural.rows);
+  // console.log(getOwnerInfosExplicit.rows);
 
   // await dbSQL.query('delete from owners');
   // const getOwnerInfos = await dbSQL.query('select * from ')
+
+  // await dbSQL.query(
+  //   'insert into sitters (user_id, description) values ($1,$2)',
+  //   [ '19', 'I like to take care of animals' ]
+  // )
+
+  // await dbSQL.query(
+  //   'insert into owners (user_id, description) values ($1,$2)',
+  //   [ '19', 'I have lots of animals' ]
+  // )
+
+  const getSitterInfosNatural = await dbSQL.query('select * from sitters natural inner join users');
+  console.log(getSitterInfosNatural.rows);
+
+  // await dbSQL.query(
+  //   'insert into pets (owner_id) values ($1)',
+  //   [ '11' ]
+  // )
+
+  const getPets = await dbSQL.query('select * from pets');
+  console.log(getPets.rows);
+
+  await dbSQL.query('insert into spaces (name, description, sitter_id) values($1, $2, $3)', ['jardin', '50m2 carre', '1']);
+  const allSpaces = await dbSQL.query('select * from spaces');
+  console.log(allSpaces.rows);
+
+  // This should work :
+  // await dbSQL.query('insert into images(url, user_id) values($1, $2)', ['www.monimage.com', '17']);
+  // This shouldn't work :
+  // await dbSQL.query('insert into images(url, user_id, space_id) values($1, $2, $3)', ['www.monimage.com', '3', '8']);
+  // const getImages =  await dbSQL.query('select * from images');
+  // console.log(getImages);
+  // const delImages = await dbSQL.query('delete from images');
 
 })();
